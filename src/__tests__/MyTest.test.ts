ff9ceb99d32cca3b2e6ae34498fd9c9eaa9bf6df
basel-ui <105730412+basel-ui@users.noreply.github.com>
2022-11-20 14:46:24 +0100

    neue ts funktion

--- src/__tests__/MyTest.test.ts
+++ src/__tests__/MyTest.test.ts
@@ -1,6 +1,6 @@
   describe('Meine Tests', () => {
     it('Mein erster Test', () => {
         expect(true).toBe(true);
-        expect(process.env.MEINE_UMGEBUNGS_VARIABLE).toEqual("Test1234")
+        expect(process.env.MEINE_UMGEBUNGS_VARIABLE).not.toEqual("Test1234")
     });
 });
